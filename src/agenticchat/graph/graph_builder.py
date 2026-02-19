66fbf0e56fd6ca008bc174d462c553f3635e1d6b
Raphael <raphamotta2015@gmail.com>
2026-02-19 15:09:07 -0300

    feature: pipeline for workflow building with a graph builder for modularization

---
+++ src/agenticchat/graph/graph_builder.py
@@ -0,0 +1,20 @@
+from langgraph.graph import StateGraph, START, END
+from src.agenticchat.state.state import State 
+from src.agenticchat.nodes.basic_chatbot_node import BasicChatbotNode
+
+
+class GraphBuilder:
+    def __init__(self, model):
+        self.llm = model
+        self.graph_builder=StateGraph(State)
+
+    def basic_chatbot_build_graph(self):        
+        """
+        Builds a basic chatbot using LangGraph.
+        This method initializes a chatbot node.
+        """
+        self.basic_chatbot_node=BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot", self.basic_chatbot_node.process)
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_edge("chatbot", END)
