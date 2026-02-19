75a4ac70932e0b72258408e47847296c2fa93e62
Raphael <raphamotta2015@gmail.com>
2026-02-19 16:10:42 -0300

    feature: chat start running based on users choices before start

--- src/agenticchat/main.py
+++ src/agenticchat/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from src.agenticchat.graph.graph_builder import GraphBuilder
 from src.agenticchat.ui.streamlitui.load_ui import LoadStreamLitUi
+from src.agenticchat.LLMS.groq import GroqConnection
+from src.agenticchat.ui.streamlitui.display_result import DisplayResultStreamLit
 
 def load_agenticchat_app():
     """
@@ -14,3 +17,31 @@ def load_agenticchat_app():
         return
 
     user_message = st.chat_input("Enter your message:")
+
+    if user_message:
+        try:
+            obj_llm_config=GroqConnection(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+
+            if not model:
+                st.error("Llm model could not be initialized")
+                return 
+            
+            usecase=user_input.get("selected_usecase")
+
+            if not usecase:
+                st.error("Wrong usecase")
+                return
+            
+            graph_builder=GraphBuilder(model)
+            try: 
+                graph=graph_builder.setup_graph(usecase)
+                DisplayResultStreamLit(usecase, graph, user_message).display_result_on_ui()
+
+            except Exception as e:
+                st.error(f"Error: Graph set up failed {e}")
+                return
+
+        except Exception as e:
+            st.error(f"Error: Graph set up failed {e}")
+            return
